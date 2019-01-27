5c059c4fb8a73d2aa21edf2c37484f2b409c56c9
Louis Mollick <louismollick@gmail.com>
2019-01-27 10:28:01 -0500

    final

--- index.js
+++ index.js
@@ -1,39 +1,51 @@
 let items = [
     {
         "name": "Mozerella cheese",
+        "description": "A slice of cheese from italy it's pretty gooood",
         "locationx": 14,
-        "locationy": 15
+        "locationy": 15,
+        "image" : "/images/cheese.jpg",
+        "price": "2.95$"
     } ,
     {
         "name": "Sliced white bread",
+        "description": "BREEEAAAAAADDDDDDDD",
         "locationx" : 313/10.5,
-        "locationy" : 212/8.11
+        "locationy" : 212/8.11,
+        "image" : "/images/bread.jpg",
+        "price": "3.95$"
     },
     {
         "name": "Doritos",
+        "description": "For the big of bone",
         "locationx": 335/10.5 ,
-        "locationy": 373/8.11
+        "locationy": 373/8.11,
+        "image" : "/images/doritos.png",
+        "price": "5.95$"
     },
     {
         "name": "Frozen bread",
+        "description": "Why would anyone buy frozen bread",
         "locationx" : 850/10.5 ,
-        "locationy":  123/8.11
+        "locationy":  123/8.11,
+        "image" : "/images/fbread.jpg",
+        "price": "4.95$"
     },
     {
         "name": "Canned tuna",
+        "description": "Tuna sandwich incoming",
         "locationx": 373/10.5 ,
-        "locationy" : 250/8.11
+        "locationy" : 250/8.11,
+        "image" : "/images/tuna.jpg",
+        "price": "0.95$"
     },
     {
         "name": "Almonds",
+        "description": "Nuts n stuff, the worst kind tho",
         "locationx": 260/10.5,
-        "locationy" : 303/8.11
-    },
-
-    {
-        "name": "Sliced cheese",
-        "locationx" : 205/10.5 ,
-        "locationy": 130/8.11
+        "locationy" : 303/8.11,
+        "image" : "/images/almonds.jpeg",
+        "price": "0.55$"
     }
     ];
 
@@ -50,18 +62,33 @@ function setPin(name){
     items.forEach(item => {
         if (name === item.name){
             let container = document.createElement('div');
-            let pin = document.createElement('img');
-            pin.className="click";
-            pin.setAttribute("src","https://img.icons8.com/color/32/000000/map-pin.png");
             container.style.position = "absolute";
             container.style.top = `${item.locationy}%`;
             container.style.left = `${item.locationx}%`;
-            container.className = "pin popover data-toggle=\"popover\" title=\"Popover Header\" data-content=\"Some content inside the popover\"";
+            let pin = document.createElement('img');
+            pin.className="click position-absolute";
+            pin.setAttribute("src","https://img.icons8.com/color/32/000000/map-pin.png");
             let popover = document.createElement('div');
-            popover.className = 'yeet d-none';
-            popover.innerHTML = "<div class=\"popuptext\" id=\"myPopup\">Popup text...</div>";
-            container.appendChild(pin);
+            popover.className = 'yeet position-absolute d-none';
+            popover.innerHTML = `<div class="popuptext" id="myPopup">
+                                    <h5><u>${item.name}</u></h5>
+                                    <div class="container">
+                                        <div class="row">
+                                            <div class="col-sm">
+                                                <img src="${item.image}" width=100px ></img>
+                                            </div>
+                                            <div class="col-sm">
+                                                <p>
+                                                <u>Description:</u><br>
+                                                ${item.description}<br>
+                                                <u>Price: </u> ${item.price}
+                                                </p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>`;
             container.appendChild(popover);
+            container.appendChild(pin);
             document.getElementById('map').appendChild(container);
         }
     });
